fd29258d10d6532591b3b3ae780c52b8b5400038
saltchicken <john.eicher89@gmail.com>
2025-05-31 14:15:56 -0700

    Side by side receiver controls

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,10 @@ function App() {
         <div ref={chartRef2} style={{ height: '100%', width: '100%' }} />
       </div>
       <div style={{ margin: 0, background: '#111', color: '#ccc', height: '10%', width: '100vw' }}>
-        <GainControl />
-        <CenterFreqControl />
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', height: '100%' }}>
+          <GainControl />
+          <CenterFreqControl />
+        </div>
       </div>
     </div>
   );
